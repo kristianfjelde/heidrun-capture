a9c975b45e7846f6cab9fccb3e5ac3a2e7733e12
Kristian Fjelde Pedersen <kristian@plaato.io>
2024-10-16 14:20:06 +0200

    Add logging of output states

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
 const http = require('http')
-const {parse} = require('url')
 const express = require('express')
 const app = express()
 
 const { BTCMD_GetStatus } = require('./lib/cgiByteCodeMap')
 const { config } = require('./config')
-const sanityClient = require('@sanity/client')
-
-const client = sanityClient({
-  projectId: config.projectId,
-  dataset: config.dataset,
-  token: config.token,
-  useCdn: false
-})
 
 const code = 'a'
 
 let logging = true
-
-const lastLogQuery = '*[_type == "brewLog"] | order(_createdAt desc) { _id, title }[0]'
 
 process.argv.forEach(function (val, index, array) {
   if (val == 'no-log') {
@@ -26,111 +15,62 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
-const subscription = client.listen('*[_type == "command" && !(_id in path ("drafts.**"))]')
-  .subscribe(command => {
-    console.log('response', command)
-    runRemoteCommand(command)
-  })
+function parseOutputStatus(outputStatus) {
+  let outputs = {
+    'Heat Elements': [],
+    Pumps: [],
+    Valves: [],
+  };
 
-
-function returnResultRemoteCommand(command, result) {
-  console.log('sending result', result)
-  client
-    .patch(command.result.id)
-    .setIfMissing({result})
-    .commit()
-}
-
-function runRemoteCommand(command = {}) {
-  const btnicCommand = command.result && command.result.command
-
-  console.log('command:', btnicCommand)
-
-  if (!btnicCommand) {
-    console.error('No command')
-    return
+  for (let i = 0; i < 2; i++) {
+    if (outputStatus & (1 << (11 + i))) {
+      const heating = ['HTL', 'Boil'];
+      outputs['Heat Elements'].push(heating[i]);
+    }
   }
 
-  http.get(
-    `${config.url}?${btnicCommand}`, resp => {
-      let data = ''
-      console.log('resp', resp)
-      resp.on('data', chunk => {
-        data += chunk
-      })
-      resp.on('end', () => {
-        console.log('got data', data)
-        returnResultRemoteCommand(command, data)
-      })
+  for (let i = 0; i < 3; i++) {
+    if (outputStatus & (1 << (8 + i))) {
+      const pumps = ['HTL', 'Mash', 'Boil'];
+      outputs['Pumps'].push(pumps[i]);
     }
-  ).on('error', err => {
-    console.error(`Error connecting to Brewtroller ${err.message}`)
-  })
+  }
+
+  for (let i = 0; i < 8; i++) {
+    if (outputStatus & (1 << i)) {
+      outputs['Valves'].push(i + 1);
+    }
+  }
+
+  return outputs;
 }
 
-
 async function startLog() {
-  const lastLog = await client.fetch(lastLogQuery).then(log => {
-    console.log('# Start logging')
-    console.log('title', log.title)
-    console.log('_id', log._id)
-    return log
-  })
-  
   const sendLogItem = (id, logItem) => {
-    const newLogItem = {}
-    Object.keys(logItem).forEach(key => {
-      // Convert to number
-      if (
-        key !== 'responseCode'
-        && key !== '_type'
-        && key !== '_key'
-        && key !== 'timestamp'
-        && key !== 'Program1_Name'
-        && key !== 'Program2_Name'
-        && key !== 'Program3_Name'
-        && key !== 'Mash_Zone_Active_Program_Step'
-        && key !== 'Mash_Zone_Active_Program_Recipe'
-        && key !== 'Boil_Zone_Active_Program_Step'
-        && key !== 'Boil_Zone_Active_Program_Recipe'
-      ) {
-        newLogItem[key] = Number(logItem[key])
-      } else {
-        newLogItem[key] = logItem[key]
-      }
-    })
+    const newLogItem = logItem;
 
-    // Send data to sanity
-    client
-      .patch(id)
-      .setIfMissing({log: []})
-      .append('log', [newLogItem])
-      .commit()
-      .then(updatedLog => {
-          console.log(`Updated: HLT: ${newLogItem['HLT_Temperature']} MASH: ${newLogItem['Mash_Temperature']} BOIL: ${newLogItem['Kettle_Temperature']}`)
-        })
-      .catch(err => {
-        console.error('Sanity client: Oh no, the update failed: ', err.message)
-      })
+    let outputs = parseOutputStatus(logItem['outputStatus']);
+
+    console.log('Activated Outputs:');
+    if (outputs['Heat Elements'].length > 0) {
+      console.log('Heating Elements: ' + outputs['Heat Elements'].join(', '));
+    }
+    if (outputs['Pumps'].length > 0) {
+      console.log('Pumps: ' + outputs['Pumps'].join(', '));
+    }
+    if (outputs['Valves'].length > 0) {
+      console.log('Valves: ' + outputs['Valves'].join(', '));
+    }
+
+    console.log(`Temperatures:`)
+    console.log(`HLT: ${newLogItem['HLT_Temperature']} MASH: ${newLogItem['Mash_Temperature']} BOIL: ${newLogItem['Kettle_Temperature']}`)
   }
   
   setInterval(() => {
-    // Create logitem
     const logItem = {
       _type: 'logItem',
       _key: new Date().getTime(),
       timestamp: new Date()
-    }
-  
-    // Testing with mock
-    if (config.mock) {
-      console.log('mock')
-      BTCMD_GetStatus().rspParams.map((param, i) => {
-        logItem[param] = config.mock[i]
-      })
-      logItem.HLT_Temperature = Math.random() * 10000
-      sendLogItem(lastLog._id, logItem)
-      return
     }
   
     http.get(
@@ -141,17 +81,16 @@ async function startLog() {
         })
     
         resp.on('end', () => {
-          // Map the brewtroller values
           BTCMD_GetStatus().rspParams.map((param, i) => {
             logItem[param] = JSON.parse(data)[i]
           })
 
           if (logItem['responseCode'] === code) {
             sendLogItem(lastLog._id, logItem)
-            
-          } else {
-            console.log('Got empty status from brewtroller', data)
           }
+          //else {
+          //  console.log('Got empty status from brewtroller', data)
+          //}
         })
     
       }
@@ -172,7 +111,6 @@ app.get('/', function (req, res) {
   res.send('Heidrun says hello 🍺')
 })
 
-// Proxy to the brewtroller
 app.get('/api/btnic', function (req, res) {
 
   res.header("Access-Control-Allow-Origin", "*")
